839f73895cb38ae1cc00e62f9cd5f378d7da01d3
IgorRocha <iogorrochamiranda2@gmail.com>
2025-12-15 02:59:08 -0300

    Troca do nome da função externa e correção do ".export"

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,7 +3,7 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
+function makeAdder() {
   let soma = 0;
 
   return function adder(numero) {
@@ -21,4 +21,4 @@ function makeInfinityAdder() {
   };
 }
 
-module.exports = makeInfinityAdder;
+module.exports = makeAdder;
