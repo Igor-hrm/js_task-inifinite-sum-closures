f2f650cb288ee69e71ace9c07960f0dd8b5467af
IgorRocha <iogorrochamiranda2@gmail.com>
2025-12-15 02:44:12 -0300

    Solução para soma com clossures com chamada com mais parâmetros. (Zerando contador ao final)

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,21 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let soma = 0;
+
+  return function proximo(numero) {
+    if (numero !== undefined) {
+      soma += numero;
+
+      return proximo;
+    } else {
+      const somaAtual = soma;
+
+      soma = 0;
+
+      return somaAtual;
+    }
+  };
 }
 
 module.exports = makeInfinityAdder;
